e1f5257e755c7d64c3f6fcedcd56f59b3583cc33
Ali Raza <aliraza315508@gmail.com>
2026-01-29 11:26:55 -0500

    pushing basic setup

---
+++ spring-cloud-config-sever/src/test/java/com/in28minutes/microservices/spring_cloud_config_sever/SpringCloudConfigSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.spring_cloud_config_sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
